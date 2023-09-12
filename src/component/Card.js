8c7a39c41940bed2bc96cd8029a150f62c3ea2e7
adekunle22k <106889375+adekunle22k@users.noreply.github.com>
2023-09-12 12:49:00 +0100

    v.1.0.0.0

--- src/component/Card.js
+++ src/component/Card.js
@@ -73,126 +73,122 @@ function Card() {
 
         <motion.div className="service_card" variants={container} initial="hidden" exit="exit" whileInView="show" viewport={{once:false}}>
 
-            <motion.div className="card" variants={item}>
-                <motion.span className="service_icon" style={{backgroundColor:"#ddfbf9"}} variants={hoverEffect} whileHover="whileHover" whileTap='whileTap'>
-                    <IconContext.Provider value={{color:"#14da8f", size:"22px"}}>
-                        <IoMailOutline/>
-                    </IconContext.Provider>
-                </motion.span>
-                <h3>Ui/Ux Design<br/>For Mobile & Web</h3>
-                <a href="#">
-                    <span>learn more</span>
-                    <IconContext.Provider value={{color:"#14da8f", size:"18px"}}>
-                        <IoChevronForward/>
-                    </IconContext.Provider>
-                </a>
-            </motion.div>
+<motion.div className="card" variants={item}>
+    <motion.span className="service_icon" style={{backgroundColor:"#ddfbf9"}} variants={hoverEffect} whileHover="whileHover" whileTap='whileTap'>
+        <IconContext.Provider value={{color:"#14da8f", size:"22px"}}>
+            <IoMailOutline/>
+        </IconContext.Provider>
+    </motion.span>
+    <h3>Hair cuts<br/>And Hair-styling.</h3>
+    <a href="#">
+        <span>learn more</span>
+        <IconContext.Provider value={{color:"#14da8f", size:"18px"}}>
+            <IoChevronForward/>
+        </IconContext.Provider>
+    </a>
+</motion.div>
 
-            <motion.div className="card" variants={item}>
-                <motion.span className="service_icon" style={{backgroundColor:"#e7daf8"}} variants={hoverEffect} whileHover="whileHover" whileTap='whileTap'>
-                    <IconContext.Provider value={{color:"#5700cf", size:"22px"}}>
-                        <IoApps/>
-                    </IconContext.Provider>
-                </motion.span>
-                <h3>Web & Mobile<br/>App Development</h3>
-                <a href="#">
-                    <span>learn more</span>
-                    <IconContext.Provider value={{color:"#14da8f", size:"18px"}}>
-                        <IoChevronForward/>
-                    </IconContext.Provider>
-                </a>
-            </motion.div>
-            <motion.div className="card" variants={item}>
-                <motion.span className="service_icon" style={{backgroundColor:"#ffede6"}} variants={hoverEffect} whileHover="whileHover" whileTap='whileTap'>
-                    <IconContext.Provider value={{color:"#ff8559", size:"22px"}}>
-                        <IoColorFill/>
-                    </IconContext.Provider>
-                </motion.span>
-                <h3>Illustration Design<br/>(Flat, 3d & More)</h3>
-                <a href="#">
-                    <span>learn more</span>
-                    <IconContext.Provider value={{color:"#14da8f", size:"18px"}}>
-                        <IoChevronForward/>
-                    </IconContext.Provider>
-                </a>
-            </motion.div>
-            <motion.div className="card" variants={item}>
-                <motion.span className="service_icon" style={{backgroundColor:"#ffe1e9"}} variants={hoverEffect} whileHover="whileHover" whileTap='whileTap'>
-                    <IconContext.Provider value={{color:"#fa3970", size:"22px"}}>
-                        <IoNotifications/>
-                    </IconContext.Provider>
-                </motion.span>
-                <h3>Stratagy & Product<br/>Management</h3>
-                <a href="#">
-                    <span>learn more</span>
-                    <IconContext.Provider value={{color:"#14da8f", size:"18px"}}>
-                        <IoChevronForward/>
-                    </IconContext.Provider>
-                </a>
-            </motion.div>
-            <motion.div className="card" variants={item}>
-                <motion.span className="service_icon" style={{backgroundColor:"#dcedff"}} variants={hoverEffect} whileHover="whileHover" whileTap='whileTap'>
-                    <IconContext.Provider value={{color:"#56a8f4", size:"22px"}}>
-                        <IoNewspaper/>
-                    </IconContext.Provider>
-                </motion.span>
-                <h3>Custome Wordpress<br/>Design & Dev.</h3>
-                <a href="#">
-                    <span>learn more</span>
-                    <IconContext.Provider value={{color:"#14da8f", size:"18px"}}>
-                        <IoChevronForward/>
-                    </IconContext.Provider>
-                </a>
-            </motion.div>
-            <motion.div className="card" variants={item}>
-                <motion.span className="service_icon" style={{backgroundColor:"#dbf9ed"}} variants={hoverEffect} whileHover="whileHover" whileTap='whileTap'>
-                    <IconContext.Provider value={{color:"#06d786", size:"22px"}}>
-                        <IoPieChart/>
-                    </IconContext.Provider>
-                </motion.span>
-                <h3>Digital Marketing<br/>& Management</h3>
-                <a href="#">
-                    <span>learn more</span>
-                    <IconContext.Provider value={{color:"#14da8f", size:"18px"}}>
-                        <IoChevronForward/>
-                    </IconContext.Provider>
-                </a>
-            </motion.div>
-            <motion.div className="card" variants={item}>
-                <motion.span className="service_icon" style={{backgroundColor:"#fffada"}} variants={hoverEffect} whileHover="whileHover" whileTap='whileTap'>
-                    <IconContext.Provider value={{color:"#f1df11", size:"22px"}}>
-                        <IoCard/>
-                    </IconContext.Provider>
-                </motion.span>
-                <h3>Branding Design<br/>(Logo & Packaging)</h3>
-                <a href="#">
-                    <span>learn more</span>
-                    <IconContext.Provider value={{color:"#14da8f", size:"18px"}}>
-                        <IoChevronForward/>
-                    </IconContext.Provider>
-                </a>
-            </motion.div>
-            <motion.div className="card dark" variants={item}>
-                <img src={process.env.PUBLIC_URL + '/images/line.png'} alt="line" className="line"/>               
-                <h2>+4 <br/>More...</h2>
-                <a href="#">
-                    <span>View more...</span>
-                    <motion.span className="service_icon" style={{backgroundColor:"#14da8f"}} variants={hoverEffect} whileHover="whileHover" whileTap='whileTap'>
-                        <IconContext.Provider value={{color:"#fff", size:"18px"}}>
-                            <IoChevronForward/>
-                        </IconContext.Provider>
-                    </motion.span>
-                </a>
-            </motion.div>
-        </motion.div>
+<motion.div className="card" variants={item}>
+    <motion.span className="service_icon" style={{backgroundColor:"#e7daf8"}} variants={hoverEffect} whileHover="whileHover" whileTap='whileTap'>
+        <IconContext.Provider value={{color:"#5700cf", size:"22px"}}>
+            <IoApps/>
+        </IconContext.Provider>
+    </motion.span>
+    <h3>Manicure<br/>And Pedicure's</h3>
+    <a href="#">
+        <span>learn more</span>
+        <IconContext.Provider value={{color:"#14da8f", size:"18px"}}>
+            <IoChevronForward/>
+        </IconContext.Provider>
+    </a>
+</motion.div>
+<motion.div className="card" variants={item}>
+    <motion.span className="service_icon" style={{backgroundColor:"#ffede6"}} variants={hoverEffect} whileHover="whileHover" whileTap='whileTap'>
+        <IconContext.Provider value={{color:"#ff8559", size:"22px"}}>
+            <IoColorFill/>
+        </IconContext.Provider>
+    </motion.span>
+    <h3>Facial's<br/>(different variants)</h3>
+    <a href="#">
+        <span>learn more</span>
+        <IconContext.Provider value={{color:"#14da8f", size:"18px"}}>
+            <IoChevronForward/>
+        </IconContext.Provider>
+    </a>
+</motion.div>
+<motion.div className="card" variants={item}>
+    <motion.span className="service_icon" style={{backgroundColor:"#ffe1e9"}} variants={hoverEffect} whileHover="whileHover" whileTap='whileTap'>
+        <IconContext.Provider value={{color:"#fa3970", size:"22px"}}>
+            <IoNotifications/>
+        </IconContext.Provider>
+    </motion.span>
+    <h3>Spa's<br/>And Massage's</h3>
+    <a href="#">
+        <span>learn more</span>
+        <IconContext.Provider value={{color:"#14da8f", size:"18px"}}>
+            <IoChevronForward/>
+        </IconContext.Provider>
+    </a>
+</motion.div>
+<motion.div className="card" variants={item}>
+    <motion.span className="service_icon" style={{backgroundColor:"#dcedff"}} variants={hoverEffect} whileHover="whileHover" whileTap='whileTap'>
+        <IconContext.Provider value={{color:"#56a8f4", size:"22px"}}>
+            <IoNewspaper/>
+        </IconContext.Provider>
+    </motion.span>
+    <h3>Waxing<br/>(Different variants)</h3>
+    <a href="#">
+        <span>learn more</span>
+        <IconContext.Provider value={{color:"#14da8f", size:"18px"}}>
+            <IoChevronForward/>
+        </IconContext.Provider>
+    </a>
+</motion.div>
+<motion.div className="card" variants={item}>
+    <motion.span className="service_icon" style={{backgroundColor:"#dbf9ed"}} variants={hoverEffect} whileHover="whileHover" whileTap='whileTap'>
+        <IconContext.Provider value={{color:"#06d786", size:"22px"}}>
+            <IoPieChart/>
+        </IconContext.Provider>
+    </motion.span>
+    <h3>Makeup<br/>(& Edges laying)</h3>
+    <a href="#">
+        <span>learn more</span>
+        <IconContext.Provider value={{color:"#14da8f", size:"18px"}}>
+            <IoChevronForward/>
+        </IconContext.Provider>
+    </a>
+</motion.div>
+<motion.div className="card" variants={item}>
+    <motion.span className="service_icon" style={{backgroundColor:"#fffada"}} variants={hoverEffect} whileHover="whileHover" whileTap='whileTap'>
+        <IconContext.Provider value={{color:"#f1df11", size:"22px"}}>
+            <IoCard/>
+        </IconContext.Provider>
+    </motion.span>
+    <h3>Hair Coloring<br/>(& Hair Styling)</h3>
+    <a href="#">
+        <span>learn more</span>
+        <IconContext.Provider value={{color:"#14da8f", size:"18px"}}>
+            <IoChevronForward/>
+        </IconContext.Provider>
+    </a>
+</motion.div>
+<motion.div className="card dark" variants={item}>
+    <img src={process.env.PUBLIC_URL + '/images/line.png'} alt="line" className="line"/>               
+    <h2>+4 <br/>More...</h2>
+    <a href="#">
+        <span>View more...</span>
+        <motion.span className="service_icon" style={{backgroundColor:"#14da8f"}} variants={hoverEffect} whileHover="whileHover" whileTap='whileTap'>
+            <IconContext.Provider value={{color:"#fff", size:"18px"}}>
+                <IoChevronForward/>
+            </IconContext.Provider>
+        </motion.span>
+    </a>
+</motion.div>
+</motion.div>
 
-    </motion.div>
-  )
+</motion.div>
+)
 }
 
 export default Card
 
-
-
-//thanks for watching
-//please subscribe my channel..........
